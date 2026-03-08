a81df82872ddc7234cd8405cb82fcd042409b105
InvokitPro <engineeringstenis@gmail.com>
2026-03-08 15:26:07 +0000

    latest upgrade for app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'invokit-v6';
+const CACHE = 'invokit-v8';
 
 self.addEventListener('install', e => {
   e.waitUntil(
@@ -7,7 +7,11 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
